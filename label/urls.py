60b9e47cb3d854ee208316b0be19986b52367424
andrew.z <a.zolotov@arktech.ai>
2024-05-16 22:53:42 +0300

    added templates for tables (very WIP)

--- label/urls.py
+++ label/urls.py
@@ -4,4 +4,13 @@ from django.urls import include, re_path
 
 urlpatterns = [
     re_path(r'^$', views.index, name='index'),
+    path('members/', views.members, name='members'),
+    path('music_bands/', views.music_bands, name='music_bands'),
+    path('concert_halls/', views.concert_halls, name='concert_halls'),
+    path('member_roles/', views.member_roles, name='member_roles'),
+    path('concert_hall_contracts/', views.concert_hall_contracts, name='concert_hall_contracts'),
+    path('concert_hall_managers/', views.concert_hall_managers, name='concert_hall_managers'),
+    path('concert_programs/', views.concert_programs, name='concert_programs'),
+    path('member_to_music_bands/', views.member_to_music_bands, name='member_to_music_bands'),
+    path('music_band_contracts/', views.music_band_contracts, name='music_band_contracts'),
 ]
--- label/views.py
+++ label/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.views import generic
-from .models import Members, MemberRoles, MemberToMusicBand, MusicBand, MusicBandContract, ConcertHallContract, ConcertHall, ConcertHallManager, ConcertProgram
-
+from .models import (
+    Members, MusicBand, ConcertHall, MemberRoles, ConcertHallContract,
+    ConcertHallManager, ConcertProgram, MemberToMusicBand, MusicBandContract
+)
 # Create your views here.
 
 def index(request):
@@ -10,3 +12,39 @@ def index(request):
         request,
         'index.html',
     )
+
+def members(request):
+    data = Members.objects.all()
+    return render(request, 'members.html', {'data': data})
+
+def music_bands(request):
+    data = MusicBand.objects.all()
+    return render(request, 'music_bands.html', {'data': data})
+
+def concert_halls(request):
+    data = ConcertHall.objects.all()
+    return render(request, 'concert_halls.html', {'data': data})
+
+def member_roles(request):
+    data = MemberRoles.objects.all()
+    return render(request, 'member_roles.html', {'data': data})
+
+def concert_hall_contracts(request):
+    data = ConcertHallContract.objects.all()
+    return render(request, 'concert_hall_contracts.html', {'data': data})
+
+def concert_hall_managers(request):
+    data = ConcertHallManager.objects.all()
+    return render(request, 'concert_hall_managers.html', {'data': data})
+
+def concert_programs(request):
+    data = ConcertProgram.objects.all()
+    return render(request, 'concert_programs.html', {'data': data})
+
+def member_to_music_bands(request):
+    data = MemberToMusicBand.objects.all()
+    return render(request, 'member_to_music_bands.html', {'data': data})
+
+def music_band_contracts(request):
+    data = MusicBandContract.objects.all()
+    return render(request, 'music_band_contracts.html', {'data': data})
