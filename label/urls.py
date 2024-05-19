6db57ed7d7922ba52eb9fb3cd3d0c945ce46eb4a
andrew.z <a.zolotov@arktech.ai>
2024-05-19 20:22:55 +0300

    all cruds done

--- label/urls.py
+++ label/urls.py
@@ -13,8 +13,41 @@ urlpatterns = [
     path('concert_programs/', views.concert_programs, name='concert_programs'),
     path('member_to_music_bands/', views.member_to_music_bands, name='member_to_music_bands'),
     path('music_band_contracts/', views.music_band_contracts, name='music_band_contracts'),
+
     path('members/edit/<int:members_id>/', views.edit_member, name='edit_member'),
     path('members/create/', views.create_member, name='create_member'),
     path('members/delete/<int:members_id>/', views.delete_member, name='delete_member'),
+
+    path('concert_hall_contracts/edit/<int:concert_hall_contract_id>/', views.edit_concert_hall_contract, name='edit_concert_hall_contract'),
+    path('concert_hall_contracts/create/', views.create_concert_hall_contract, name='create_concert_hall_contract'),
+    path('concert_hall_contracts/delete/<int:concert_hall_contract_id>/', views.delete_concert_hall_contract, name='delete_concert_hall_contract'),
+
+    path('concert_hall_managers/edit/<int:concert_hall_manager_id>/', views.edit_concert_hall_manager, name='edit_concert_hall_manager'),
+    path('concert_hall_managers/create/', views.create_concert_hall_manager, name='create_concert_hall_manager'),
+    path('concert_hall_managers/delete/<int:concert_hall_manager_id>/', views.delete_concert_hall_manager, name='delete_concert_hall_manager'),
+
+    path('concert_halls/edit/<int:concert_hall_id>/', views.edit_concert_hall, name='edit_concert_hall'),
+    path('concert_halls/create/', views.create_concert_hall, name='create_concert_hall'),
+    path('concert_halls/delete/<int:concert_hall_id>/', views.delete_concert_hall, name='delete_concert_hall'),
+
+    path('concert_programs/edit/<int:concert_program_id>/', views.edit_concert_program, name='edit_concert_program'),
+    path('concert_programs/create/', views.create_concert_program, name='create_concert_program'),
+    path('concert_programs/delete/<int:concert_program_id>/', views.delete_concert_program, name='delete_concert_program'),
+
+    path('member_roles/edit/<int:member_roles_id>/', views.edit_member_role, name='edit_member_role'),
+    path('member_roles/create/', views.create_member_role, name='create_member_role'),
+    path('member_roles/delete/<int:member_roles_id>/', views.delete_member_role, name='delete_member_role'),
+
+    path('member_to_music_bands/edit/<int:member_to_music_band_id>/', views.edit_member_to_music_band, name='edit_member_to_music_band'),
+    path('member_to_music_bands/create/', views.create_member_to_music_band, name='create_member_to_music_band'),
+    path('member_to_music_bands/delete/<int:member_to_music_band_id>/', views.delete_member_to_music_band, name='delete_member_to_music_band'),
+
+    path('music_band_contracts/edit/<int:music_band_contract_id>/', views.edit_music_band_contract, name='edit_music_band_contract'),
+    path('music_band_contracts/create/', views.create_music_band_contract, name='create_music_band_contract'),
+    path('music_band_contracts/delete/<int:music_band_contract_id>/', views.delete_music_band_contract, name='delete_music_band_contract'),
+
+    path('music_band/edit/<int:music_band_id>/', views.edit_music_band, name='edit_music_band'),
+    path('music_band/create/', views.create_music_band, name='create_music_band'),
+    path('music_band/delete/<int:music_band_id>/', views.delete_music_band, name='delete_music_band'),
 
 ]
--- label/views.py
+++ label/views.py
@@ -4,7 +4,10 @@ from .models import (
     Members, MusicBand, ConcertHall, MemberRoles, ConcertHallContract,
     ConcertHallManager, ConcertProgram, MemberToMusicBand, MusicBandContract
 )
-from .forms import MemberForm
+from .forms import (
+    MemberForm, MusicBandForm, ConcertHallForm, MemberRolesForm, ConcertHallContractForm,
+    ConcertHallManagerForm, ConcertProgramForm, MemberToMusicBandForm, MusicBandContractForm
+)
 # Create your views here.
 
 def index(request):
@@ -77,3 +80,228 @@ def delete_member(request, members_id):
         member.delete()
         return redirect('members')
     return redirect('members')
+
+def edit_concert_hall_contract(request, concert_hall_contract_id):
+    concert_hall_contracts = get_object_or_404(ConcertHallContract, concert_hall_contract_id=concert_hall_contract_id)
+    if request.method == "POST":
+        form = ConcertHallContractForm(request.POST, instance=concert_hall_contracts)
+        if form.is_valid():
+            form.save()
+            return redirect('concert_hall_contracts')
+    else:
+        form = ConcertHallContractForm(instance=concert_hall_contracts)
+    return render(request, 'edit_concert_hall_contract.html', {'form': form})
+
+def create_concert_hall_contract(request):
+    if request.method == "POST":
+        form = ConcertHallContractForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('concert_hall_contracts')
+    else:
+        form = ConcertHallContractForm()
+    return render(request, 'create_concert_hall_contract.html', {'form': form})
+
+def delete_concert_hall_contract(request, concert_hall_contract_id):
+    concert_hall_contracts = get_object_or_404(ConcertHallContract, concert_hall_contract_id=concert_hall_contract_id)
+    if request.method == "POST":
+        concert_hall_contracts.delete()
+        return redirect('concert_hall_contracts')
+    return redirect('concert_hall_contracts')
+
+def edit_concert_hall_manager(request, concert_hall_manager_id):
+    concert_hall_managers = get_object_or_404(ConcertHallManager, concert_hall_manager_id=concert_hall_manager_id)
+    if request.method == "POST":
+        form = ConcertHallManagerForm(request.POST, instance=concert_hall_managers)
+        if form.is_valid():
+            form.save()
+            return redirect('concert_hall_managers')
+    else:
+        form = ConcertHallManagerForm(instance=concert_hall_managers)
+    return render(request, 'edit_concert_hall_manager.html', {'form': form})
+
+def create_concert_hall_manager(request):
+    if request.method == "POST":
+        form = ConcertHallManagerForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('concert_hall_managers')
+    else:
+        form = ConcertHallManagerForm()
+    return render(request, 'create_concert_hall_manager.html', {'form': form})
+
+def delete_concert_hall_manager(request, concert_hall_manager_id):
+    concert_hall_managers = get_object_or_404(ConcertHallManager, concert_hall_manager_id=concert_hall_manager_id)
+    if request.method == "POST":
+        concert_hall_managers.delete()
+        return redirect('concert_hall_managers')
+    return redirect('concert_hall_managers')
+
+def edit_concert_hall(request, concert_hall_id):
+    concert_hall = get_object_or_404(ConcertHall, concert_hall_id=concert_hall_id)
+    if request.method == "POST":
+        form = ConcertHallForm(request.POST, instance=concert_hall)
+        if form.is_valid():
+            form.save()
+            return redirect('concert_halls')
+    else:
+        form = ConcertHallForm(instance=concert_hall)
+    return render(request, 'edit_concert_hall.html', {'form': form})
+
+def create_concert_hall(request):
+    if request.method == "POST":
+        form = ConcertHallForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('concert_halls')
+    else:
+        form = ConcertHallForm()
+    return render(request, 'create_concert_hall.html', {'form': form})
+
+def delete_concert_hall(request, concert_hall_id):
+    concert_hall = get_object_or_404(ConcertHall, concert_hall_id=concert_hall_id)
+    if request.method == "POST":
+        concert_hall.delete()
+        return redirect('concert_halls')
+    return redirect('concert_halls')
+
+def edit_concert_program(request, concert_program_id):
+    concert_program = get_object_or_404(ConcertProgram, concert_program_id=concert_program_id)
+    if request.method == "POST":
+        form = ConcertProgramForm(request.POST, instance=concert_program)
+        if form.is_valid():
+            form.save()
+            return redirect('concert_programs')
+    else:
+        form = ConcertProgramForm(instance=concert_program)
+    return render(request, 'edit_concert_program.html', {'form': form})
+
+def create_concert_program(request):
+    if request.method == "POST":
+        form = ConcertProgramForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('concert_programs')
+    else:
+        form = ConcertProgramForm()
+    return render(request, 'create_concert_program.html', {'form': form})
+
+def delete_concert_program(request, concert_program_id):
+    concert_program = get_object_or_404(ConcertProgram, concert_program_id=concert_program_id)
+    if request.method == "POST":
+        concert_program.delete()
+        return redirect('concert_programs')
+    return redirect('concert_programs')
+
+def edit_member_role(request, member_roles_id):
+    member_role = get_object_or_404(MemberRoles, member_roles_id=member_roles_id)
+    if request.method == "POST":
+        form = MemberRolesForm(request.POST, instance=member_role)
+        if form.is_valid():
+            form.save()
+            return redirect('member_roles')
+    else:
+        form = MemberRolesForm(instance=member_role)
+    return render(request, 'edit_member_role.html', {'form': form})
+
+def create_member_role(request):
+    if request.method == "POST":
+        form = MemberRolesForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('member_roles')
+    else:
+        form = MemberRolesForm()
+    return render(request, 'create_member_role.html', {'form': form})
+
+def delete_member_role(request, member_roles_id):
+    member_role = get_object_or_404(MemberRoles, member_roles_id=member_roles_id)
+    if request.method == "POST":
+        member_role.delete()
+        return redirect('member_roles')
+    return redirect('member_roles')
+
+def edit_member_to_music_band(request, member_to_music_band_id):
+    member_to_music_band = get_object_or_404(MemberToMusicBand, member_to_music_band_id=member_to_music_band_id)
+    if request.method == "POST":
+        form = MemberToMusicBandForm(request.POST, instance=member_to_music_band)
+        if form.is_valid():
+            form.save()
+            return redirect('member_to_music_bands')
+    else:
+        form = MemberToMusicBandForm(instance=member_to_music_band)
+    return render(request, 'edit_member_to_music_band.html', {'form': form})
+
+def create_member_to_music_band(request):
+    if request.method == "POST":
+        form = MemberToMusicBandForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('member_to_music_bands')
+    else:
+        form = MemberToMusicBandForm()
+    return render(request, 'create_member_to_music_band.html', {'form': form})
+
+def delete_member_to_music_band(request, member_to_music_band_id):
+    member_to_music_band = get_object_or_404(MemberToMusicBand, member_to_music_band_id=member_to_music_band_id)
+    if request.method == "POST":
+        member_to_music_band.delete()
+        return redirect('member_to_music_bands')
+    return redirect('member_to_music_bands')
+
+def edit_music_band_contract(request, music_band_contract_id):
+    music_band_contract = get_object_or_404(MusicBandContract, music_band_contract_id=music_band_contract_id)
+    if request.method == "POST":
+        form = MusicBandContractForm(request.POST, instance=music_band_contract)
+        if form.is_valid():
+            form.save()
+            return redirect('music_band_contracts')
+    else:
+        form = MusicBandContractForm(instance=music_band_contract)
+    return render(request, 'edit_music_band_contract.html', {'form': form})
+
+def create_music_band_contract(request):
+    if request.method == "POST":
+        form = MusicBandContractForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('music_band_contracts')
+    else:
+        form = MusicBandContractForm()
+    return render(request, 'create_music_band_contract.html', {'form': form})
+
+def delete_music_band_contract(request, music_band_contract_id):
+    music_band_contract = get_object_or_404(MusicBandContract, music_band_contract_id=music_band_contract_id)
+    if request.method == "POST":
+        music_band_contract.delete()
+        return redirect('music_band_contracts')
+    return redirect('music_band_contracts')
+
+def edit_music_band(request, music_band_id):
+    music_band = get_object_or_404(MusicBand, music_band_id=music_band_id)
+    if request.method == "POST":
+        form = MusicBandForm(request.POST, instance=music_band)
+        if form.is_valid():
+            form.save()
+            return redirect('music_bands')
+    else:
+        form = MusicBandForm(instance=music_band)
+    return render(request, 'edit_music_band.html', {'form': form})
+
+def create_music_band(request):
+    if request.method == "POST":
+        form = MusicBandForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('music_bands')
+    else:
+        form = MusicBandForm()
+    return render(request, 'create_music_band.html', {'form': form})
+
+def delete_music_band(request, music_band_id):
+    music_band = get_object_or_404(MusicBand, music_band_id=music_band_id)
+    if request.method == "POST":
+        music_band.delete()
+        return redirect('music_bands')
+    return redirect('music_bands')
+
